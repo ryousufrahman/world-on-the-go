cd12774c23aa646dacb1d0a878fb96a2b76ab505
yousufrahman <ryousuf766@gmail.com>
2026-03-15 16:05:13 +0600

    added the conditional rendering and handled error

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -4,15 +4,22 @@ import Country from './Country';
 const Countries = ({countriesPromise}) => {
     const countriesData = use(countriesPromise)
       const countries =countriesData.countries
-      console.log(countries);
+    //   console.log(countries);
       
     return (
-        <div className='container'>
+        
+        <>
+        <h2>All countries : {countries.length}</h2>
+         <div className='container'>
+            
             
             {
-                countries.map(country => <Country country ={country}></Country>)
+                countries.map(country => <Country key={country.ccn3.ccn3}  country ={country}></Country>)
+                
             }
         </div>
+        
+        </>
     );
 };
 
--- src/Components/Countries/Country.jsx
+++ src/Components/Countries/Country.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
 const Country = ({country}) => {
+    // console.log(country.population.population);
+    
     return (
         <div className='country'>
+            
             <img className='country-flag'  src={country.flags.flags.png} alt={country.flags.flags.alt} />
-            <h3>Name : {country.name.official} </h3>
+            <h2>Name : {country.name.common} </h2>
+            <p>Population : {country.population.population}</p>
+            <p>{country.population.population >1000000? "big by population" : "small by population"}</p>
         </div>
     );
 };
