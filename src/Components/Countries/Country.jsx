db6e6e86fbe591abcec6c1f15a88a655281f6d40
yousufrahman <ryousuf766@gmail.com>
2026-03-15 16:51:56 +0600

    added togole button of visited

--- src/Components/Countries/Country.jsx
+++ src/Components/Countries/Country.jsx
@@ -1,17 +1,39 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
 
-const Country = ({country}) => {
-    // console.log(country.population.population);
-    
-    return (
-        <div className='country'>
-            
-            <img className='country-flag'  src={country.flags.flags.png} alt={country.flags.flags.alt} />
-            <h2>Name : {country.name.common} </h2>
-            <p>Population : {country.population.population}</p>
-            <p>{country.population.population >1000000? "big by population" : "small by population"}</p>
-        </div>
-    );
+const Country = ({ country }) => {
+  // console.log(country.population.population);
+  const [visited, setVisited] = useState(false);
+  const handleVisitBtn = () => {
+    if (visited) {
+        setVisited(false)
+        
+    }
+    else{
+        setVisited(true)
+    }
+  };
+
+  return (
+    <div className="country">
+      <img
+        className="country-flag"
+        src={country.flags.flags.png}
+        alt={country.flags.flags.alt}
+      />
+      <h2>Name : {country.name.common} </h2>
+      <h3>Capital : {country.capital.capital}</h3>
+      <p>Population : {country.population.population}</p>
+      <p>
+        {country.population.population > 1000000
+          ? "big by population"
+          : "small by population"}
+      </p>
+      <p>Area : {country.area.area}</p>
+      <p>{country.area.area > 100000 ? "Big" : "Small"}</p>
+      <button onClick={handleVisitBtn}>{visited? 'Visited' :"Not Visited"}</button>
+    </div>
+  );
 };
 
 export default Country;
