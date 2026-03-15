77a090a809aedcdea97ceed5acfb506a6c0c9c03
yousufrahman <ryousuf766@gmail.com>
2026-03-15 11:15:07 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+import Countries from './Components/Countries/Countries'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+          <h1>React is loading...</h1>
+          <Countries></Countries>
+          
+        </div>
+     
+
+
+    </>
+  )
+}
+
+export default App
